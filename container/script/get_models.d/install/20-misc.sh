77670df203457a92e58ea2d5f35948f849af944e
Zhenyu Liang <zhenyu.liang@intel.com>
2022-05-12 19:06:03 +0800

    Fix fcgi_classification_tf inference error

--- container/script/get_models.d/install/20-misc.sh
+++ container/script/get_models.d/install/20-misc.sh
@@ -11,6 +11,7 @@ mkdir -p $dest_dir
 files="
 20180402-114759-vggface2.pt
 inception_v3_2016_08_28_frozen.pb
+imagenet_slim_labels.txt
 resnet18.pt
 resnet18.txt
 scut_ept_char_list.txt
@@ -22,5 +23,3 @@ alexnet.labels
 for f in $files; do
 	cp -a $src_dir/$f $dest_dir
 done
-
-popd
