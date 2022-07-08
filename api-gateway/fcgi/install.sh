a978c55eda62c81e31e1564a5925223c0485a729
Biao Lu <biao.lu@intel.com>
2022-07-08 17:36:18 +0800

    api-gateway: remove python/conf from install.sh
    move python service and service configuration file installation
    from install.sh to CMakeLists.txt
    
    Signed-off-by: Biao Lu <biao.lu@intel.com>

--- api-gateway/fcgi/install.sh
+++ api-gateway/fcgi/install.sh
@@ -8,51 +8,34 @@ if [ $# != 1 ]; then
 fi
 
 DESTDIR=$(readlink -e $1)
-echo $DESTDIR
 if [ "x$DESTDIR" = "x" ]; then
 	echo 'DESTDIR dose not exist'
 	exit 1
 fi
 
 DESTDIR_DEVEL=$DESTDIR/../devel_rootfs
-
 MYSCRIPT_DIR=$(cd $(dirname $0)>/dev/null 2>&1;pwd -P)
-echo $MYSCRIPT_DIR
 
+#dependence
+cp -f $MYSCRIPT_DIR/../../health-monitor/health-monitor/service_runtime_health_monitor.proto ./
 
+mkdir -p $MYSCRIPT_DIR/build
+pushd $MYSCRIPT_DIR/build
+
+source /opt/intel/openvino_2022/setupvars.sh
+cmake -DCMAKE_FIND_ROOT_PATH=$DESTDIR_DEVEL ..
+make -j$(nproc --all) VERBOSE=1
+make DESTDIR=$DESTDIR install/strip
+
+popd
+
+# lighttpd config
 mkdir -p $DESTDIR/etc/lighttpd/
 cp $MYSCRIPT_DIR/lighttpd.conf $DESTDIR/etc/lighttpd/
 cp $MYSCRIPT_DIR/auth-conf.sh $DESTDIR/etc/lighttpd/
 
-# install fcgi config files
-mkdir -p $DESTDIR/etc/lighttpd/conf-available
-mkdir -p $DESTDIR/opt/fcgi/cgi-bin/
-capability_service_conf_files="\
-16-apiinfo.conf \
-16-cpuinfo.conf \
-16-gpuinfo.conf \
-16-meminfo.conf \
-"
-
-if [ "x$CONFIG_RESTFUL_CAPABILITY" = "xy" ]; then
-    for conf in ${capability_service_conf_files}; do
-        cp $MYSCRIPT_DIR/info/cpp/$conf $DESTDIR/etc/lighttpd/conf-available/
-    done
-fi
-
 
 if [ "x$CONFIG_RESTFUL_SAMPLE" = "xy" ]; then
-    for conf in `find . -name "16-*.conf"`; do
-        cp $conf $DESTDIR/etc/lighttpd/conf-available/
-    done
-
-    for pydir in `find . -type d -name "python" -not -path "./build/*"`; do
-        sample_pydir="$pydir"
-        if [ -e $sample_pydir/fcgi_*.py ]; then
-            cp $sample_pydir/fcgi_*.py $DESTDIR/opt/fcgi/cgi-bin/
-        fi
-    done
-
     if [ -d "$MYSCRIPT_DIR/asr/python/" ]; then
         mkdir -p $DESTDIR/opt/fcgi/cgi-bin/asr/
         mkdir -p $DESTDIR/opt/fcgi/cgi-bin/asr/ctcdecode_numpy/
@@ -61,8 +44,12 @@ if [ "x$CONFIG_RESTFUL_SAMPLE" = "xy" ]; then
 fi
 
 # enable default services
-###############################################################################
-
+capability_service_conf_files="\
+16-apiinfo.conf \
+16-cpuinfo.conf \
+16-gpuinfo.conf \
+16-meminfo.conf \
+"
 
 mkdir -p $DESTDIR/etc/lighttpd/conf-enabled
 pushd $DESTDIR/etc/lighttpd/conf-enabled
@@ -70,9 +57,9 @@ pushd $DESTDIR/etc/lighttpd/conf-enabled
 enabled_list="\
 ${capability_service_conf_files} \
 16-classification.conf \
-16-classification-tf.conf \
-16-face-detection.conf \
-16-facial-landmark.conf \
+16-classification_tf.conf \
+16-face_detection.conf \
+16-facial_landmark.conf \
 16-digitalnote-py.conf \
 16-tts-py.conf \
 16-policy-py.conf \
@@ -85,16 +72,6 @@ for conf in $enabled_list; do
         ln -sf ../conf-available/$conf
     fi
 done
-popd
-
-cp -f $MYSCRIPT_DIR/../../health-monitor/health-monitor/service_runtime_health_monitor.proto ./
-mkdir -p $MYSCRIPT_DIR/build
-pushd $MYSCRIPT_DIR/build
-
-source /opt/intel/openvino_2022/setupvars.sh
-cmake -DCMAKE_FIND_ROOT_PATH=$DESTDIR_DEVEL ..
-make -j$(nproc --all) VERBOSE=1
-make DESTDIR=$DESTDIR install/strip
 
 popd
 
