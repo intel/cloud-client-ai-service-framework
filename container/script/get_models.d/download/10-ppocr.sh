95f5c23cb303e56fb19c68bab672af25b49a3a7c
lisa.shi <lisa.shi@intel.com>
2022-07-27 09:38:39 +0800

    modify the download path of ppocr_keys
    Signed-off-by: lisa.shi <lisa.shi@intel.com>

--- container/script/get_models.d/download/10-ppocr.sh
+++ container/script/get_models.d/download/10-ppocr.sh
@@ -7,8 +7,7 @@ output_dir=$cache_dir/ppocr/
 mkdir -p $output_dir
 pushd $output_dir
 
-
-wget -c https://github.com/PaddlePaddle/PaddleOCR/blob/release/2.4/ppocr/utils/ppocr_keys_v1.txt
+wget -c https://raw.githubusercontent.com/PaddlePaddle/PaddleOCR/release/2.4/ppocr/utils/ppocr_keys_v1.txt
 wget -c https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_rec_infer.tar
 wget -c https://paddleocr.bj.bcebos.com/dygraph_v2.0/ch/ch_ppocr_mobile_v2.0_det_infer.tar
 tar xvf ch_ppocr_mobile_v2.0_det_infer.tar
